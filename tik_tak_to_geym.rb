f0808ad967337c9fae28a158c20017f6584db4bd
kalmansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-06-02 18:27:42 +0800

    Completed tic tac toe game

--- tik_tak_to_geym.rb
+++ tik_tak_to_geym.rb
@@ -1,26 +1,33 @@
 class Player
-    def initialize
+    attr_reader :player, :piece
+    def initialize(player, piece)
+        @player = player
+        @piece = piece
+    end  
+    def player_details
+        print "\nPlayer #{self.player} turn, you have '#{self.piece}' piece.\n\n"
     end
-    def place_piece(position)
+    def place_piece
+        position = gets.chomp
         if ["a1","a2","a3","b1","b2","b3","c1","c2","c3"].any? {|square| square == position.downcase}
-            position
+            [self.player, self.piece, position]
         else
-            false
+            [self.player, self.piece, false]
         end
     end
 end
 
 class Board
-    attr_accessor :board, :board_copy
+    attr_accessor :board
     def initialize
         @board = {a1:" ", a2:" ", a3:" ", b1:" ", b2:" ", b3:" ", c1:" ", c2:" ", c3:" "}
-        print "\n            1   2   3\n
-        A  [#{self.board[:a1]}] [#{self.board[:a2]}] [#{self.board[:a3]}]\n
-        B  [#{self.board[:b1]}] [#{self.board[:b2]}] [#{self.board[:b3]}]\n
-        C  [#{self.board[:c1]}] [#{self.board[:c2]}] [#{self.board[:c3]}]\n\n"
+        self.printboard
     end
     def updateboard(position, player_piece)
         self.board[position.to_sym] = player_piece
+        self.printboard
+    end
+    def printboard
         print "\n            1   2   3\n
         A  [#{self.board[:a1]}] [#{self.board[:a2]}] [#{self.board[:a3]}]\n
         B  [#{self.board[:b1]}] [#{self.board[:b2]}] [#{self.board[:b3]}]\n
@@ -29,56 +36,64 @@ class Board
     def is_position_not_taken?(position)
         self.board[position.to_sym] == " "
     end
-    def win_condition
+    def win_condition(player_piece)
+        (self.board[:a1] == player_piece && self.board[:a2] == player_piece && self.board[:a3] == player_piece ||
+        self.board[:b1] == player_piece && self.board[:b2] == player_piece && self.board[:b3] == player_piece ||
+        self.board[:c1] == player_piece && self.board[:c2] == player_piece && self.board[:c3] == player_piece ||
+
+        self.board[:a1] == player_piece && self.board[:b1] == player_piece && self.board[:c1] == player_piece ||
+        self.board[:a2] == player_piece && self.board[:b2] == player_piece && self.board[:c2] == player_piece ||
+        self.board[:a3] == player_piece && self.board[:b3] == player_piece && self.board[:c3] == player_piece ||
+
+        self.board[:a1] == player_piece && self.board[:b2] == player_piece && self.board[:c3] == player_piece ||
+        self.board[:a3] == player_piece && self.board[:b2] == player_piece && self.board[:c1] == player_piece)
     end
 end
 
 class Game
-    attr_accessor :player1, :player2, :board, :win_condition,:playerplaying
+    attr_accessor :player1, :player2, :board,:playerplaying
     def initialize
-        @player1 = Player.new
-        @player2 = Player.new
+        @player1 = Player.new(1, "X")
+        @player2 = Player.new(2, "O")
         @board = Board.new
-        @win_condition = WinCondition.new
         @playerplaying = rand(1..2) 
 
-        print "Game Start!!!\n"
-        print "Player #{self.playerplaying} first.\n"
+        print "\nPlayer #{self.playerplaying} first.\n"
 
-        self.player_turn(self.playerplaying)
+        self.player_turn
     end
-    def player_turn(playerplaying)
-        case playerplaying
+    def player_turn
+        case self.playerplaying
         when 1
-            print "Player 1, pick a square. (e.g. A1)\n\n"
-            play_move = self.player1.place_piece(gets.chomp)
-            if play_move == false
-                print "\nInvalid position.\n\n"
-                self.player_turn(1)
-            elsif self.board.is_position_not_taken?(play_move)
-                self.board.updateboard(play_move, "X")
-                self.player_turn(2)
-            else
-                print "\nPosition is already taken.\n\n"
-                self.player_turn(1)
-            end
+            self.player1.player_details
+            player_move = self.player1.place_piece #will return and array of [player, piece, position]
+            self.player_action(player_move)
+            self.playerplaying = 2
         when 2
-            print "Player 2, pick a square. (e.g. A1)\n\n"
-            play_move = self.player2.place_piece(gets.chomp)
-            if play_move == false
-                print "\nInvalid position.\n\n"
-                self.player_turn(2)
-            elsif self.board.is_position_not_taken?(play_move)
-                self.board.updateboard(play_move, "O")
-                self.player_turn(1)
-            else
-                print "\nPosition is already taken.\n\n"
-                self.player_turn(2)
-            end
+            self.player2.player_details
+            player_move = self.player2.place_piece
+            self.player_action(player_move)
+            self.playerplaying = 1
+        end
+        self.board.win_condition(player_move[1]) ? declare_winner : self.player_turn
+    end
+    def player_action(player_move)
+        if player_move[2] == false
+            self.board.printboard
+            print "\nInvalid Position!\n"
+            self.player_turn
+        elsif !self.board.is_position_not_taken?(player_move[2])
+            self.board.printboard
+            print "\nPosition already taken.\n"
+            self.player_turn
+        elsif player_move[2] != false && self.board.is_position_not_taken?(player_move[2])
+            self.board.updateboard(player_move[2], player_move[1])
+            print "\n...\n"
         end
     end
-    def check_winner
-        
+    def declare_winner
+        self.board.printboard
+        print "\nPlayer #{self.playerplaying} wins!!!\n\n"
     end
 end
 
